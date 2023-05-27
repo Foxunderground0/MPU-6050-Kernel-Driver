8c7ac2f4f9cbf9555aaa571dd8a62076ce29f870
Foxunderground0 <umerirfan1205@gmail.com>
2023-05-27 14:45:50 +0100

    Updates

--- Userspace_I2C_Device_Check/i2c_in_userspace.c
+++ Userspace_I2C_Device_Check/i2c_in_userspace.c
@@ -5,44 +5,64 @@
 #include <linux/i2c-dev.h>
 #include <i2c/smbus.h>
 
+int addr = 0x68; /* The I2C address of the MPU */
+int file;
+
+unsigned char readMPU(unsigned char reg);  // Function prototype
+
 int main() {
-	int file;
-	int adapter_nr = 1; /* probably dynamically determined */
+	int adapter_nr = 1;
 	char filename[20];
 
 	snprintf(filename, 19, "/dev/i2c-%d", adapter_nr);
+
 	file = open(filename, O_RDWR);
+
 	if (file < 0) {
-		perror("Failed to open the I2C bus");
+		printf("[Error] Failed to open the I2C bus\n");
 		return 1;
 	}
-
-	// Further code for I2C communication and transactions would follow here
-
-	int addr = 0x68; /* The I2C address */
 
 	if (ioctl(file, I2C_SLAVE, addr) < 0) {
-		/* ERROR HANDLING; you can check errno to see what went wrong */	
+		printf("[Error] Unable to set the i2c address\n");
 		return 1;
 	}
 
-	printf("Made it this far 1 \n");
 
-	__u8 reg = 0x75; /* Device register to access */
-	__u8 res;
-	char buf[10];
-
-	/* Using SMBus commands */
-	res = i2c_smbus_read_word_data(file, reg);
-	if (res < 0) {
-		printf("SMB Failed \n");
-		/* ERROR HANDLING: I2C transaction failed */
-	} else {
-		printf("Made It this far 2 \n");
-		/* res contains the read word */
-		printf("%d", res);
+	while (1){
+		printf("%d\n", readMPU(0x3f));
 	}
 
 	close(file); // Close the opened I2C device
 	return 0;
+}
+
+
+
+unsigned char writeMPU(unsigned char reg){
+	unsigned char res;
+
+	/* Using SMBus command */
+	res = i2c_smbus_read_word_data(file, reg);
+	if (res < 0) {
+		printf("Reading From: %d failed\n", reg);
+	} else {
+		//printf("%d", res);
+		return res;
+	}
+
+}
+
+unsigned char readMPU(unsigned char reg){
+	unsigned char res;
+
+	/* Using SMBus command */
+	res = i2c_smbus_read_word_data(file, reg);
+	if (res < 0) {
+		printf("Reading From: %d failed\n", reg);
+	} else {
+		//printf("%d", res);
+		return res;
+	}
+
 }
